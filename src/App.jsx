717ab10b467d957a87e2ef8b6e1f56c631ffe279
pekoy <mhmmdfaiz160@gmail.com>
2024-03-25 20:18:12 +0700

    feat: add click-able button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import './App.css'
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
   return (
    <div className="App">
       <h1>My todos</h1>
@@ -20,8 +21,8 @@ function App() {
           </div>
         </div>
         <div className="btn-area">
-          <button>Todo</button>
-          <button>Completed</button>
+          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>Todo</button>
+          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)}>Completed</button>
         </div>
         <div className="todo-list">
           <div className="todo-list-item">
